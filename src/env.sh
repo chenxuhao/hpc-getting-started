0a2c4b81775601dd4cc32006a01f02653d40943d
chenxuhao <cxh@mit.edu>
2024-07-26 09:01:28 -0500

    update env.sh

--- src/env.sh
+++ src/env.sh
@@ -1,2 +1,11 @@
+# on Delta
 module load papi
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/papi/7.0.1.2/lib
+export PAPI_HOME=/opt/cray/pe/papi/7.0.1.2
+
+# on Rockfish
+#export PAPI_HOME=/scratch4/xhchen/papi/src/install
+
+# On Anvil
+#export PAPI_HOME=/anvil/projects/x-cis220095/papi/src/install
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PAPI_HOME/lib
